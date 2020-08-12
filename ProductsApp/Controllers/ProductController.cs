505aee0bfb8a81f5fa051e3e125d7845fa539d71
ilya <beqasmile@gmail.com>
2020-08-12 18:47:11 +0300

    Products App

---
+++ ProductsApp/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        [HttpGet]
+        [ActionName("GetAllProducts")]
+        public HttpResponseMessage GetAllProductsHTTP()
+        {
+            HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK, products);
+
+            return httpResponseMessage;
+        }
+        [HttpGet]
+        [ActionName("AllMyProducts")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            Product[] myProducts = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+           
+        };
+            return myProducts;
+        }
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+
+    }
+}
