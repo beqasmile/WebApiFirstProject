21b8d55f3b5c4edbb62f13176a03217814829460
ilya <beqasmile@gmail.com>
2020-08-12 20:01:06 +0300

    Add products list

--- ProductsApp/Controllers/ProductController.cs
+++ ProductsApp/Controllers/ProductController.cs
@@ -10,38 +10,50 @@ namespace ProductsApp.Controllers
 {
     public class ProductController : ApiController
     {
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-        };
+        //Product[] products = new Product[]
+        //{
+        //    new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+        //    new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+        //    new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        //};
 
         [HttpGet]
         [ActionName("GetAllProducts")]
         public HttpResponseMessage GetAllProductsHTTP()
         {
-            HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK, products);
+            HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK, ProductsModel.GetProducts().Products);
 
             return httpResponseMessage;
         }
-        [HttpGet]
-        [ActionName("AllMyProducts")]
-        public IEnumerable<Product> GetAllProducts()
+ 
+
+
+        [HttpPost]
+        [ActionName("AddProduct")]
+        public IHttpActionResult AddProduct(Product product)
         {
-            Product[] myProducts = new Product[]
-        {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-           
-        };
-            return myProducts;
+            if (product != null)
+            {
+                var existProduct = ProductsModel.GetProducts().Products.FirstOrDefault(p => p.Id == product.Id);
+                if (existProduct != null)
+                {
+                    throw new Exception("Product already exists");
+                }
+                ProductsModel.GetProducts().Products.Add(product);
+                return Ok(product);
+            }
+            return NotFound();
+            
         }
-        public IHttpActionResult GetProduct(int id)
+
+        [HttpGet]
+        [ActionName("GetProductById")]
+        public IHttpActionResult GetProductById(int id)
         {
-            var product = products.FirstOrDefault((p) => p.Id == id);
+            var product = ProductsModel.GetProducts().Products.FirstOrDefault((p) => p.Id == id);
             if (product == null)
             {
-                return NotFound();
+                throw new Exception("Product doesnot exists");
             }
             return Ok(product);
         }
---
+++ ProductsApp/Models/ProductsModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ProductsApp.Models
+{
+    public class ProductsModel
+    {
+        private List<Product> products;
+
+        public static ProductsModel _instance;
+
+        public List<Product> Products { get => products; set => products = value; }
+
+        public ProductsModel()
+        {
+            this.Products = new List<Product>() {
+                new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+                new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+                new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            };
+        }
+
+        public static ProductsModel GetProducts()
+        {
+            if (_instance == null)
+            {
+                _instance = new ProductsModel();
+            }
+
+            return _instance;
+        }
+
+
+
+
+
+    }
+}
